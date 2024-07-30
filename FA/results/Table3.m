dd5196912e73231c5a538af248561b26ef05694f
Leah South <leah.south@hdr.qut.edu.au>
2024-07-30 16:13:47 +1000

    Adding compute times to the FA runs

--- FA/results/Table3.m
+++ FA/results/Table3.m
@@ -4,4 +4,5 @@ load("results.mat");
 for m=1:3
     mean([ksd_ata_smc(:,m) ksd_ans_smc(:,m) ksd_ns_smc(:,m) ksd_ns(:,m)])
     mean([count_ata_smc(:,m) count_ans_smc(:,m) count_ns_smc(:,m) count_ns(:,m)])
+    mean([time_ata_smc(:,m) time_ans_smc(:,m) time_ns_smc(:,m) time_ns(:,m)])
 end
--- FA/results/combine_results.m
+++ FA/results/combine_results.m
@@ -3,24 +3,29 @@
 % Table 3.
 
 count_ns = NaN(100,3);
+time_ns = NaN(100,3);
 log_evidence_ns = NaN(100,3);
 log_evidence_ns_star = NaN(100,3);
 ksd_ns = NaN(100,3);
 ksd_ns_star = NaN(100,3);
 
 count_ans_smc = NaN(100,3);
+time_ans_smc = NaN(100,3);
 log_evidence_ans_smc = NaN(100,3);
 ksd_ans_smc = NaN(100,3);
 
 count_ata_smc = NaN(100,3);
+time_ata_smc = NaN(100,3);
 log_evidence_ata_smc = NaN(100,3);
 ksd_ata_smc = NaN(100,3);
 
 count_ns_smc = NaN(100,3);
+time_ns_smc = NaN(100,3);
 log_evidence_ns_smc = NaN(100,3);
 ksd_ns_smc = NaN(100,3);
 
 count_ta_smc = NaN(100,3);
+time_ta_smc = NaN(100,3);
 log_evidence_ta_smc = NaN(100,3);
 ksd_ta_smc = NaN(100,3);
 
@@ -29,6 +34,7 @@ for m = 1:3
         filename = sprintf("NS_m%d_run%d.mat",m,rep);
         load(filename);
         count_ns(rep,m) = count_loglike;
+        time_ns(rep,m) = time;
         log_evidence_ns(rep,m) = log_evidence;
         log_evidence_ns_star(rep,m) = log_evidence_star;
         ksd_ns(rep,m) = KSD_NS;
@@ -37,30 +43,34 @@ for m = 1:3
         filename = sprintf("ANS_SMC_m%d_run%d.mat",m,rep);
         load(filename);
         count_ans_smc(rep,m) = count_loglike;
+        time_ans_smc(rep,m) = time;
         log_evidence_ans_smc(rep,m) = log_evidence;
         ksd_ans_smc(rep,m) = KSD;
         
         filename = sprintf("ATA_SMC_m%d_run%d.mat",m,rep);
         load(filename);
         count_ata_smc(rep,m) = count_loglike;
+        time_ata_smc(rep,m) = time;
         log_evidence_ata_smc(rep,m) = log_evidence;
         ksd_ata_smc(rep,m) = KSD;
         
         filename = sprintf("NS_SMC_m%d_run%d.mat",m,rep);
         load(filename);
         count_ns_smc(rep,m) = count_loglike + count_ans_smc(rep,m); % Because we did adaptive run first
+        time_ns_smc(rep,m) = time + time_ans_smc(rep,m); % Because we did adaptive run first
         log_evidence_ns_smc(rep,m) = log_evidence;
         ksd_ns_smc(rep,m) = KSD;
         
         filename = sprintf("TA_SMC_m%d_run%d.mat",m,rep);
         load(filename);
         count_ta_smc(rep,m) = count_loglike + count_ata_smc(rep,m); % Because we did adaptive run first
+        time_ta_smc(rep,m) = time + time_ata_smc(rep,m); % Because we did adaptive run first
         log_evidence_ta_smc(rep,m) = log_evidence;
         ksd_ta_smc(rep,m) = KSD;
     end
 end
 
-clearvars -except count_* log_evidence_* ksd_*
+clearvars -except count_* time_* log_evidence_* ksd_*
 clear log_evidence_star
 
 save("results.mat");
